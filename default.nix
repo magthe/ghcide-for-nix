afad1f55768eb50d412d9da1ee8489d80673f6b8
Magnus Therning <magnus@therning.org>
2020-06-01 16:48:56 +0200

    Pin to newer Nixpkgs

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = import (builtins.fetchTarball {
-    name = "nixpkgs-unstable-2020-04-06";
-    url = https://github.com/NixOS/nixpkgs-channels/archive/ae6bdcc53584aaf20211ce1814bea97ece08a248.tar.gz;
-    sha256 = "sha256:0hjhznns1cxgl3hww2d5si6vhy36pnm53hms9h338v6r633dcy77";
+    name = "nixpkgs-unstable-2020-06-01";
+    url = https://github.com/NixOS/nixpkgs-channels/archive/868ad77793f535b061b5a38c4881a34c1b2379e4.tar.gz;
+    sha256 = "sha256:1g9ca1052jywnj12d8khdllnla9dgvh3ip6zf0nhp3il34yg3wj7";
   }) { inherit config; };
 
   config = {
